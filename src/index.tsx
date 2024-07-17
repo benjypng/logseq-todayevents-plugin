9a4c40524acc3b1f37ab3863cc0edc6fa24cf750
Benjy <ben@pngs.cc>
2024-07-17 19:23:58 +0800

    chore: add check for valid uuid

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import '@logseq/libs'
+
+import { isValidUUID } from '@logseq/libs/dist/helpers'
+
 import { handlePopup } from './handle-popup'
 import { settings } from './settings'
 
 const main = async () => {
   console.log('<insert-plugin-name> loaded')
-
   // Used to handle any popups
   handlePopup()
+
+  if (isValidUUID(logseq.settings!.icalTemplateBlockRef)) {
+    await logseq.UI.showMsg(
+      'logseq-ical-plugin: Double-check block reference in settings',
+    )
+  }
 }
 
 logseq.useSettingsSchema(settings).ready(main).catch(console.error)
--- src/settings.ts
+++ src/settings.ts
@@ -2,10 +2,10 @@ import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user'
 
 export const settings: SettingSchemaDesc[] = [
   {
-    key: 'sampleSetting',
+    key: 'icalTemplateBlockRef',
     type: 'string',
-    default: 'This is a sample setting',
-    title: 'Sample Setting',
-    description: 'Modify settings.ts accordingly.',
+    default: 'xxx',
+    title: 'Block Reference for Template',
+    description: 'Block reference number where your template can be found.',
   },
 ]
